3cd950915295291a1d09c79a4e229d769c8574f5
sammaadaniel30 <danielsamac@gmail.com>
2025-12-26 19:53:57 +0100

    Terminado sección guía

--- docs/js/guia.js
+++ docs/js/guia.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const banner = document.getElementById("mobile-banner");
+  const closeBtn = document.getElementById("close-banner");
 
-
-
+  closeBtn.addEventListener("click", () => {
+    banner.style.display = "none";
+    document.body.style.paddingBottom = "0";
+  });
 });
+
