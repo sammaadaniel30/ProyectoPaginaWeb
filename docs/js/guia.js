f012bb79d22d3c7b881d0c9969001f40186f8460
sammaadaniel30 <danielsamac@gmail.com>
2025-12-29 11:44:45 +0100

    Permitir la reproduccion automatica de videos

--- docs/js/guia.js
+++ docs/js/guia.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  // Banner 
   const banner = document.getElementById("mobile-banner");
   const closeBtn = document.getElementById("close-banner");
 
@@ -6,5 +8,31 @@ document.addEventListener("DOMContentLoaded", () => {
     banner.style.display = "none";
     document.body.style.paddingBottom = "0";
   });
+
+  // Reproducir vídeos automaticamente al hacer scroll 
+
+  const videos = document.querySelectorAll("iframe");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const iframe = entry.target;
+      const src = iframe.dataset.src || iframe.src;
+
+      if (entry.isIntersecting) {
+        iframe.src = src + "?autoplay=1&mute=1";
+      } else {
+        iframe.src = src;
+      }
+    });
+  }, {
+    threshold: 0.5
+  });
+
+  videos.forEach(video => {
+    video.dataset.src = video.src;
+    observer.observe(video);
+  });
+
+
 });
 
